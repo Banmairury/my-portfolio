7794057181e45d3dbac41d73e25fbd16492f1126
Teerapong <banmairuruy@gmail.com>
2023-06-05 00:54:22 +0700

    add new project

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,5 +1,6 @@
 import project0 from '../images/project-2.png'
 import project1 from '../images/project-1.png'
+import project3 from '../images/project-3.png'
 
 const Project = () => {
   return (
@@ -8,7 +9,8 @@ const Project = () => {
       <div className="h-60  flex justify-around items-center">
         <div className=" bg-gray-400 w-60 h-40 rounded-lg text-center text-xl hover:-translate-y-3 ease-out duration-300 hadow-lg hover:shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)] "> <a href='https://replit.com/@TeerapongB/JSD0452-Find-Your-Hat' target='blank'><img className='rounded-lg w-60 h-40' src={project0}></img></a></div>
         <div className=" bg-gray-400 w-60 h-40 rounded-lg text-center text-xl hover:-translate-y-3 ease-out duration-300 hadow-lg hover:shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)]"> <a href='https://career-changer-react-assessment-banmairury.vercel.app/' target='blank'><img className='rounded-lg w-60 h-40' src={project1}></img></a></div>
-        <div className=" bg-gray-400 w-60 h-40 rounded-lg text-center text-xl"> <span>Coming Soon</span></div>
+        <div className=" bg-gray-400 w-60 h-40 rounded-lg text-center text-xl hover:-translate-y-3 ease-out duration-300 hadow-lg hover:shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)]"> <a href='http://128.199.131.246:5173' target='blank'><img className='rounded-lg w-60 h-40' src={project3}></img></a></div>
+        
       </div>
     </div>
   );
